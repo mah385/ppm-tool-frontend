3cc8a5c1cc7e3cb17c951411fd66475662dd7051
mah385 <santhosh.p@gofrugal.com>
2020-02-27 19:39:47 +0530

    added DashBoard.js component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import DashBoard from "./components/DashBoard";
 
 function App() {
-  return <div className="badge badge-success m-2">inside App component</div>;
+  return (
+    <div className="badge badge-success m-2">
+      <h6>inside App component</h6>
+      <hr />
+      <DashBoard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/DashBoard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class DashBoard extends Component {
+  render() {
+    return (
+      <div>
+        <h6>inside DashBoard component</h6>
+      </div>
+    );
+  }
+}
+
+export default DashBoard;
